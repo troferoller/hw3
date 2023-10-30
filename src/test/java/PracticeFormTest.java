44742c7715023b5737de3b080f3193418748d65f
troferoller <d.dulger@mail.ru>
2023-10-30 19:38:58 +0300

    deleted unused statemant

--- src/test/java/PracticeFormTest.java
+++ src/test/java/PracticeFormTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeAll;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
+
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
